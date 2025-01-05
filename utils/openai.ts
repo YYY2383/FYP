9ffc2809f1bc2cc342ba04f3dfd55741ef31d677
Yi Yap <yxy397@student.bham.ac.uk>
2025-01-05 06:40:08 +0000

    reconfig2

--- utils/openai.ts
+++ utils/openai.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, 
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(req: Request, res: NextResponse) {
@@ -10,7 +10,7 @@ export async function POST(req: Request, res: NextResponse) {
 
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: body.messages }],
+    messages: body.messages,
   });
   console.log(completion.choices[0].message);
   const theResponse = completion.choices[0].message;

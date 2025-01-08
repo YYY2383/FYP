cc3e578091bfb96c517ea240f55e6fe71b6a317f
Yi Yap <yxy397@student.bham.ac.uk>
2025-01-08 17:13:13 +0000

    fix loading

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react"
+import Navbar from "../components/navbar"
+
+
+export default function Home() {
+  
+
+  return (
+    <main className='main'>
+      <div className='container'>
+        <h1 className="appName">Food Compiler</h1>
+        
+        <div className = 'body'>
+          <div>
+            <Navbar />
+          </div>
+
+          <div className = 'head'>
+            <h2>This is the HOME PAGE!!!!!</h2>
+            
+          </div>
+        </div>
+      </div>
+
+      
+    </main>
+  )
+}

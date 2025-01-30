7ad8e99fa67aa5690ab94ff8c4f15086cd571f31
Yi Yap <yxy397@student.bham.ac.uk>
2025-01-30 19:34:18 +0000

    fix push problem

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React, {useEffect, useState} from "react"
 import Navbar from "../components/navbar"
 import {db} from './firebaseConfig'
